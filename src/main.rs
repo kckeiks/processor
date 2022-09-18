92f83653c4306f3ece8b4e248caa46d5f9788357
Miguel Guarniz <mi9uel9@gmail.com>
2022-09-18 12:00:22 -0400

    Read csv files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::error::Error;
+use std::io;
+use std::process;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    #[serde(rename = "type")]
+    ty: String,
+    client: u32,
+    tx: u16,
+    amount: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    for result in rdr.deserialize() {
+        let record: Record = match result {
+            Ok(r) => r,
+            Err(err) => {
+                println!("error: {}", err);
+                process::exit(1);
+            }
+        };
+        println!("{:?}", record);
+    }
 }
